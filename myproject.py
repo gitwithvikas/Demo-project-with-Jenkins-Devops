ccc3ba857b8d2302d628c605cfb1335a091d4015
gitwithvikas <98875396+gitwithvikas@users.noreply.github.com>
2023-08-03 15:05:32 +0530

    initail

---
+++ myproject.py
@@ -0,0 +1,21 @@
+#check any key is present or not in dictionary
+dis = {'first':'Govind',"second":'sonu'}
+
+if 'third' in dis:
+    print('Present in dics')
+else:
+    print('Not Present')   
+   
+    
+#count the number any list
+lst = [11,22,33,44,55,66,11]
+print(lst.count(11))
+tpl = (22,33,5,55,4,3,2,33)
+print(tpl.count(33))
+    
+    
+# find index in list
+
+lst = [22,33,44,55,66,77]
+
+print(lst.index(44))    
